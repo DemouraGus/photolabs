3dc649a4845cf8d65bf07a16eddf58d90ad3b841
DemouraGus <demoura.gus@gmail.com>
2025-01-09 09:32:01 -0800

    Mock data imported and used by PhotoList and TopicList

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import '../styles/HomeRoute.scss'
 import TopNavigation from "components/TopNavigationBar";
 import PhotoList from "components/PhotoList";
+import photos from 'mocks/photos'
 
 const HomeRoute = () => {
   return (
     <div className="home-route">
       <TopNavigation />
-      <PhotoList />
+      <PhotoList photos={photos}/>
     </div>
   )
 }
