f9cc7f6513c24886d518b36400d0857a522c7513
DemouraGus <demoura.gus@gmail.com>
2025-01-09 15:52:44 -0800

    Photo details passed to modal component

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -6,9 +6,36 @@ import closeSymbol from '../assets/closeSymbol.svg';
 const PhotoDetailsModal = ({ photo, onClose }) => {
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button" onClick={onClose} >
-        <img src={closeSymbol} alt="close symbol" />
+      <button className="photo-details-modal__close-button" onClick={onClose}>
+        <img src={closeSymbol} alt="Close" />
       </button>
+      <div className="photo-details-modal__top-bar">
+        <div className="photo-details-modal__photographer-details">
+          <img
+            className="photo-details-modal__photographer-profile"
+            src={photo.user.profile}
+            alt={`${photo.user.name}'s profile`}
+          />
+          <div>
+            <p className="photo-details-modal__photographer-info">
+              {photo.user.name}
+            </p>
+            <p className="photo-details-modal__photographer-location">
+              {photo.location.city}, {photo.location.country}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="photo-details-modal__images">
+        <img
+          className="photo-details-modal__image"
+          src={photo.urls.full}
+          alt={photo.description || 'Photo'}
+        />
+      </div>
+      <p className="photo-details-modal__header">
+        {photo.description || 'No description available.'}
+      </p>
     </div>
   )
 };
